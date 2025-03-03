330ae441e05eb0737a4af45a4085737f784da114
tomchan <tokman.chan@gmail.com>
2025-03-03 19:34:49 +1030

    init: tailwindcss changed to nuxt plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,8 @@
-import tailwindcss from "@tailwindcss/vite"
 import { defineNuxtConfig } from "nuxt/config"
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ]
-  }
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
 })
