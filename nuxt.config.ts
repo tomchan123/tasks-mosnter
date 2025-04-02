3e28e771e11ba57af4cbbd08c6c7a2683136ec8d
tomchan <tokman.chan@gmail.com>
2025-04-02 18:36:59 +1030

    feat: add tailwindcss and shadnc ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon']
-})
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    prefix: "",
+    componentDir: "./components/ui",
+  },
+});
+
