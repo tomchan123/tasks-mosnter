91f06aa9feb02b149cde82eae42d400adda388fc
tomchan <tokman.chan@gmail.com>
2025-04-06 22:04:06 +0930

    feat: add new app layout

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-  <NavigationBar />
+  <div
+    class="bg-muted h-screen flex flex-col items-center justify-between font-grotesk p-4 gap-4"
+  >
+    <div class="container-content flex-1 self-stretch">Placeholder</div>
+    <div class="flex-none">
+      <NavigationBar />
+    </div>
+  </div>
 </template>
