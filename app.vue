7ceec200bde4f5d5b7e3bff160ed19ec65b863f9
tomchan <tokman.chan@gmail.com>
2025-04-06 23:42:43 +0930

    feat: new visuals with active and focused navbar item

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div
     class="bg-muted h-screen flex flex-col items-center justify-between font-grotesk p-4 gap-4"
   >
-    <div class="container-content flex-1 self-stretch">Placeholder</div>
+    <div class="container-content flex-1 self-stretch"><NuxtPage /></div>
     <div class="flex-none">
       <NavigationBar />
     </div>
--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -16,19 +16,19 @@ const navMenuItemData: NavMenuItemData[] = [
   <NavigationMenu
     class="bg-background rounded-xl p-2 ring-2 ring-accent shadow-md"
   >
-    <NavigationMenuList>
+    <NavigationMenuList class="group/list">
       <NavigationMenuItem v-for="item in navMenuItemData" :key="item.url">
         <NuxtLink v-slot="{ isActive, href, navigate }" :to="item.url" custom>
           <NavigationMenuLink
             :active="isActive"
             :href
-            class="group/item"
+            class=""
             @click="navigate"
           >
             <div class="flex flex-row justify-center items-center h-6 px-2">
               <Icon :name="item.iconName" class="text-xl" />
               <p
-                class="text-base max-w-0 overflow-hidden group-hover/item:max-w-xl group-hover/item:block transition-all ease-in-out duration-300 m-0 group-hover/item:ml-1"
+                class="text-base max-w-0 overflow-hidden group-hover/link:max-w-xl group-hover/link:block transition-all ease-in-out duration-300 m-0 group-hover/link:ml-1"
               >
                 {{ item.title }}
               </p>
